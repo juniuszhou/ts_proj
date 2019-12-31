88c9538f5702435dcf1433923340f670603589b8
juniuszhou <junius.zhou@gmail.com>
2019-12-31 16:26:18 +0800

    project init.

---
+++ first.ts
@@ -0,0 +1,10 @@
+class Person {
+    name: String;
+    constructor(name: String) {
+        this.name = name;
+    }
+}
+
+let person = new Person("Junius");
+console.log(person.name);
+
---
+++ readme.txt
@@ -0,0 +1,6 @@
+# install ts-node to run and debug ts 
+ts-node "/Users/junius/Documents/code/src/github.com/juniuszhou/ts_proj/first.ts"
+
+
+#
+
