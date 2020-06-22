68f8c2b76b34f8ac9a4522b867211e6dcd6231c2
juniuszhou <junius.zhou@gmail.com>
2020-06-22 11:32:33 +0800

    m

--- grammar/class_usage.ts
+++ grammar/class_usage.ts
@@ -8,3 +8,10 @@ class Person {
 let person = new Person("Junius");
 console.log(person.name);
 
+class Man extends Person {
+
+}
+
+let man = new Man("Zhou")
+console.log(man.name)
+
---
+++ grammar/types.ts
@@ -0,0 +1,36 @@
+const Hello: string = "Hello"
+console.log(Hello)
+
+let ten: number = 10
+let point: number = 0.1
+let if_ok: boolean = true
+
+let arr: number[] = [1, 2]
+let arr2: Array<number> = [1, 2]
+
+let tuple: [string, string] = ["Hello", "World"]
+
+enum Color {Red, Blue, Green}
+
+// void null undefined and any
+function get_void(): void {
+    let a: number = 1;
+}
+
+let x: any = 2;
+
+export type ApiTypes = 'promise' | 'rxjs';
+
+
+/*
+null
+在 JavaScript 中 null 表示 "什么都没有"。
+
+null是一个只有一个值的特殊类型。表示一个空对象引用。
+
+用 typeof 检测 null 返回是 object。
+
+undefined
+在 JavaScript 中, undefined 是一个没有设置值的变量。
+*/
+
